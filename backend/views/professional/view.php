2b74cea94d4f634824968df687180ff23294e84a
kalanedev <larissakalaneab@gmail.com>
2025-04-21 12:39:55 -0300

    change the grid outside of modal using yii tabs, separate professional informations and bond clinics

--- backend/views/professional/view.php
+++ backend/views/professional/view.php
@@ -1,20 +1,11 @@
 <?php
-
 use yii\helpers\Html;
 use yii\widgets\DetailView;
-use yii\bootstrap5\Modal;
 use yii\helpers\Url;
-
-/** @var yii\web\View $this */
-/** @var common\models\Professional $model */
-
-$this->title = $model->name;
-$this->params['breadcrumbs'][] = ['label' => 'Professionals', 'url' => ['index']];
-$this->params['breadcrumbs'][] = $this->title;
-\yii\web\YiiAsset::register($this);
+use yii\bootstrap5\Modal;
 ?>
-<div class="professional-view">
 
+<div class="professional-view">
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
@@ -28,72 +19,84 @@ $this->params['breadcrumbs'][] = $this->title;
         ]) ?>
     </p>
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            'name',
-            'advice',
-            'advice_number',
-            'birthdate',
-           [
-            'attribute' => 'status',
-            'value' => $model->status ? 'active' : 'inactive',
-           ]
-        ],
-    ]) ?>
-
-    <div class="row">
-        <div class="col-md-12">
-            <?= Html::button(
-                'Bond Clinics',
-                [
-                    'id' => 'show-clinics-btn',
-                    'class' => 'btn btn-primary'
-                ]
-            )
-            ?>
+    <ul class="nav nav-tabs" id="professionalTabs" role="tablist">
+        <li class="nav-item" role="presentation">
+            <button class="nav-link active" id="principal-tab" data-bs-toggle="tab" data-bs-target="#principal" 
+                    type="button" role="tab" aria-controls="principal" aria-selected="true">
+                Principal
+            </button>
+        </li>
+        <li class="nav-item" role="presentation">
+            <button class="nav-link" id="clinics-tab" data-bs-toggle="tab" data-bs-target="#clinics" 
+                    type="button" role="tab" aria-controls="clinics" aria-selected="false">
+                Clinics
+            </button>
+        </li>
+    </ul>
+    
+    <div class="tab-content" id="professionalTabsContent">
+        <div class="tab-pane fade show active p-3" id="principal" role="tabpanel" aria-labelledby="principal-tab">
+            <?= DetailView::widget([
+                'model' => $model,
+                'attributes' => [
+                    'id',
+                    'name',
+                    'advice',
+                    'advice_number',
+                    'birthdate',
+                    [
+                        'attribute' => 'status',
+                        'value' => $model->status ? 'active' : 'inactive',
+                    ]
+                ],
+            ]) ?>
+        </div>
+        
+        <div class="tab-pane fade p-3" id="clinics" role="tabpanel" aria-labelledby="clinics-tab">
+            <div id="clinics-content">
+                <div class="text-center">
+                    <div class="spinner-border" role="status">
+                        <span class="visually-hidden">Loading...</span>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
-    <?php 
-        Modal::begin([
-            'id' =>'clinics-modal',
-            'title' => '<h4>Bond clinics</h4>',
-            'size' => 'lg',
-            'options' => [
-                'tabindex' => false,
-                'aria-labelledby' => 'clinics-modal-label',
-            ],
-            'bodyOptions' => [
-                'id' => 'clinics-content',
-            ],
-        ]);
-
-        Modal::end();
-    ?>
-
     <?php
+    Modal::begin([
+        'id' => 'clinic-operation-modal',
+        'title' => '<h4>Clinic Operation</h4>',
+        'size' => 'lg',
+        'bodyOptions' => [
+            'id' => 'clinic-operation-content',
+        ],
+    ]);
+    echo '<div class="text-center"><div class="spinner-border" role="status"></div></div>';
+    Modal::end();
+    ?>
+</div>
 
-    $clinicsUrl = Url::to(['professional/clinics', 'id' => $model->id]);
+<?php
+$clinicsUrl = Url::to(['professional/clinics', 'id' => $model->id]);
 
-    $js = <<<JS
-    $('#show-clinics-btn').on('click', function() {
-        $.ajax({
-            url:'$clinicsUrl',
-            type: 'GET',
-            success: function(response){
-                $('#clinics-content').html(response);
-                var myModal = new bootstrap.Modal(document.getElementById('clinics-modal'));
-                myModal.show();
-        },
-        error: function(xhr) {
-            alert('Failed to load clinics.');
-            console.error(xhr.responseText);
+$js = <<<JS
+    $('#clinics-tab').on('click', function(e) {
+        if (!$(this).data('loaded')) {
+            $.ajax({
+                url: '{$clinicsUrl}',
+                type: 'GET',
+                success: function(response) {
+                    $('#clinics-content').html(response);
+                    $('#clinics-tab').data('loaded', true);
+                },
+                error: function(xhr, status, error) {
+                    console.error('Error loading clinics:', error);
+                    $('#clinics-content').html('<div class="alert alert-danger">Error loading clinics: ' + error + '</div>');
+                }
+            });
         }
     });
-});
-
 JS;
 
 $this->registerJs($js);
